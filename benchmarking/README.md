5112b3a20384220ea4ac3393a2d190823c044c84
fteufel <felix.teufel@gmail.com>
2021-10-19 16:12:12 +0200

    use awk

--- benchmarking/README.md
+++ benchmarking/README.md
@@ -6,9 +6,21 @@ We benchmark the following aligmnent programs:
 - CD-HIT / CD-HIT-EST
 - MMseqs2
 
+## Datasets
 
-# Installation
+Dataset   | Type    | Number of classes | Threshold | Link 
+----------|---------|-------------------|-----------|-----------------
+NetGPI    | Protein | 2                 | 0.3       |
+iLoc-mRNA | RNA     | 5                 | 0.8       |
+ |DNA|
+
+## Install additional dependencies
 ```
 conda install -c conda-forge -c bioconda mmseqs2
 conda install -c bioconda cd-hit
 ```
+
+
+## Experiments
+
+1. Ass
--- benchmarking/get_mmseqs_edgelists.sh
+++ benchmarking/get_mmseqs_edgelists.sh
@@ -8,7 +8,7 @@ mmseqs createdb data/netgpi_dataset.fasta temp/netgpi_db
 mmseqs prefilter -s 7.5 temp/netgpi_db temp/netgpi_db temp/netgpi_pref
 mmseqs align temp/netgpi_db temp/netgpi_db temp/netgpi_pref temp/netgpi_align_db
 mmseqs convertalis temp/netgpi_db temp/netgpi_db temp/netgpi_align_db temp/alignments.tab # column 3 contains sequence identity.
-paste -d "," <(cut -f1 -d "|" temp/alignments.tab) <(cut -f2 temp/alignments.tab | cut -f1 -d "|") <(cut -f3 temp/alignments.tab) >netgpi_mmseqs_edgelist.csv
+awk -F'[|\t]' '{print $1","$4","$7}' temp/alignments.tab >netgpi_mmseqs_edgelist.csv
 
 
 # 2. iLoc-mRNA
@@ -17,7 +17,7 @@ mmseqs createdb --dbtype 2 data/mloci_dataset.fasta temp/mloci_db
 mmseqs prefilter -s 7.5 temp/mloci_db temp/mloci_db temp/mloci_pref
 mmseqs align temp/mloci_db temp/mloci_db temp/mloci_pref temp/mloci_align_db
 mmseqs convertalis temp/mloci_db temp/mloci_db temp/mloci_align_db temp/alignments.tab # column 3 contains sequence identity.
-paste -d "," <(cut -f1 -d "|" temp/alignments.tab) <(cut -f2 temp/alignments.tab | cut -f1 -d "|") <(cut -f3 temp/alignments.tab) >ilocmrna_mmseqs_edgelist.csv
+awk -F'[|\t]' '{print $1","$4","$7}' temp/alignments.tab >ilocmrna_mmseqs_edgelist.csv
 
 
 rm -rf temp
