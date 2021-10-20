50f2c01392c023ad78ab62339f227d6f4a97c008
fteufel <felix.teufel@gmail.com>
2021-10-20 10:35:45 +0200

    update run script

--- webserver/run.sh
+++ webserver/run.sh
@@ -2,8 +2,8 @@
 
 mkdir output
 
-graphpart "$@" #>output/graphpart.log.txt 2>&1
-
+graphpart "$@" >output/graphpart.log.txt #2>&1
+python make_output.py
 #python generate_output.py
 
-#cat output.md
+cat output.md
