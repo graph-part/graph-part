ad2b98dbc3fdc59c2fd58a2ba5b07bb7aa806a8a
fteufel <felix.teufel@gmail.com>
2021-10-23 14:48:31 +0200

    update benchmarking bounds

--- benchmarking/hpc_scripts/get_length_runtime_benchmarks_needle.sh
+++ benchmarking/hpc_scripts/get_length_runtime_benchmarks_needle.sh
@@ -10,7 +10,7 @@
 CONDA_BASE=$(conda info --base) ; source $CONDA_BASE/etc/profile.d/conda.sh
 conda activate gp-env
 
-for LENGTH in 50 100 150 200 250 300 350 400 450 500
+for LENGTH in 50 100 150 200 250 300 350 400 450 500 550 600 650 700 850 900 950 1000
 do
 graphpart needle -ff "/zhome/1d/8/153438/experiments/graph-part/benchmarking/runtime_benchmark/${LENGTH}_aas.fasta" \
 --threshold 0.30 \
--- benchmarking/runtime_benchmark/make_size_data.sh
+++ benchmarking/runtime_benchmark/make_size_data.sh
@@ -4,7 +4,7 @@
 # When upsampling, need to change identifier names.
 # in each loop, prefix previous identifier with another letter.
 
-for PREFIX in 'A' 'B' 'C' 'D' 'E' 'F'
+for PREFIX in 'A' 'B' 'C' 'D' 'E' 'F' 'G' 'H' 'I' 'J' 'K' 'L'
 do
 sed "s/>/>${PREFIX}/" ../data/protein/netgpi_dataset.fasta >extension_${PREFIX}.fasta
 done
@@ -13,7 +13,7 @@ cat extension* >source.fasta
 cat source.fasta  | paste - - >source.tsv
 rm extension*
 
-for NUMBER in 1000 2000 3000 4000 5000 6000 7000 8000 9000 10000 11000 12000 13000 14000 15000 16000 17000 18000 19000 20000
+for NUMBER in 1000 2000 3000 4000 5000 6000 7000 8000 9000 10000 11000 12000 13000 14000 15000 16000 17000 18000 19000 20000 21000 22000 23000 24000 25000 26000 27000 28000 29000 30000
 do
 shuf -n $NUMBER source.tsv | sed 's/\t/\n/' > "${NUMBER}_seqs.fasta"
 done
@@ -30,19 +30,8 @@ done
 
 cat extension* >source.fasta
 
-# this brings us over 500k
-for PREFIX in 'A' 'B' 'C' 'D' 'E' 
-do
-sed "s/>/>${PREFIX}/" source.fasta >extension_${PREFIX}.fasta
-done
 
-cat extension* >source.fasta
-
-
-cat source.fasta  | paste - - >source.tsv
-rm extension*
-
-for NUMBER in 50000 100000 150000 200000 250000 300000 450000 500000
+for NUMBER in 35000 40000 45000 50000 55000 65000 70000 75000 80000 85000 90000 95000 100000
 do
 shuf -n $NUMBER source.tsv | sed 's/\t/\n/' > "${NUMBER}_seqs.fasta"
 done
